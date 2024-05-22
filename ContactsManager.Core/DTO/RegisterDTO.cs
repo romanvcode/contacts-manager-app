3dfccffb4748c2dff5d5fe513b8e68f3e2fe339d
romanvcode <roman.v.ua@gmail.com>
2024-05-22 14:37:11 +0300

    Add project files.

---
+++ ContactsManager.Core/DTO/RegisterDTO.cs
@@ -0,0 +1,40 @@
+﻿using ContactsManager.Core.Enums;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactsManager.Core.DTO
+{
+    public class RegisterDTO
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        public string PersonName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [DataType(DataType.EmailAddress)]
+        [Remote(action: "IsEmailAlreadyRegistered", controller: "Account", ErrorMessage = "Email is already registered")]
+        public string Email { get; set; }
+
+        [Required]
+        [RegularExpression("^[0-9]*$", ErrorMessage="Phone must contain only numbers")]
+        [DataType(DataType.PhoneNumber)]
+        public string Phone { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string ConfirmPassword { get; set; }
+
+        public UserTypeOptions UserType { get; set; } = UserTypeOptions.User;
+    }
+}
---
+++ ContactsManager.UI/Controllers/AccountController.cs
@@ -0,0 +1,180 @@
+﻿using ContactsManager.Core.Domain.IdentityEntities;
+using ContactsManager.Core.DTO;
+using ContactsManager.Core.Enums;
+using CRUDExample.Controllers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactsManager.UI.Controllers
+{
+    [Route("[controller]/[action]")]
+    // [AllowAnonymous]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly RoleManager<ApplicationRole> _roleManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, RoleManager<ApplicationRole> roleManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+        }
+
+        [HttpGet]
+        [Authorize("NotAuthentificated")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Authorize("NotAuthentificated")]
+        // [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterDTO registerDTO)
+        {
+            // Check if the model is valid
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Errors = ModelState.Values.SelectMany(x => x.Errors).Select(x => x.ErrorMessage);
+
+                return View(registerDTO);
+            }
+
+            ApplicationUser user = new ApplicationUser()
+            {
+                Email = registerDTO.Email,
+                PhoneNumber = registerDTO.Phone,
+                UserName = registerDTO.Email,
+                PersonName = registerDTO.PersonName
+            };
+
+            IdentityResult result = await _userManager.CreateAsync(user, registerDTO.Password);
+
+            if (result.Succeeded)
+            {
+                // Check status of radio button
+                if (registerDTO.UserType == UserTypeOptions.Admin)
+                {
+                    // Create admin role
+                    if (await _roleManager.FindByNameAsync(UserTypeOptions.Admin.ToString()) is null) 
+                    { 
+                        ApplicationRole applicationRole = new ApplicationRole() {
+                            Name = UserTypeOptions.Admin.ToString() 
+                        };
+
+                        await _roleManager.CreateAsync(applicationRole);
+                    }
+
+                    // Add the new user into 'Admin' role
+                    await _userManager.AddToRoleAsync(user, role: UserTypeOptions.Admin.ToString());
+                }
+                else 
+                {
+                    if (await _roleManager.FindByNameAsync(UserTypeOptions.User.ToString()) is null)
+                    {
+                        ApplicationRole applicationRole = new ApplicationRole()
+                        {
+                            Name = UserTypeOptions.User.ToString()
+                        };
+
+                        await _roleManager.CreateAsync(applicationRole);
+                    }
+
+                    // Add the new user into 'User' role
+                    await _userManager.AddToRoleAsync(user, role: UserTypeOptions.User.ToString());
+                }
+
+                // Sign in the user
+                await _signInManager.SignInAsync(user, isPersistent: false);
+
+                return RedirectToAction(nameof(PersonsController.Index), "Persons");
+            }
+            else
+            {
+                foreach (IdentityError error in result.Errors)
+                {
+                    ModelState.AddModelError("Register", error.Description);
+                }
+
+                return View(registerDTO);
+            }
+        }
+
+        [HttpGet]
+        [Authorize("NotAuthentificated")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Authorize("NotAuthentificated")]
+        public async Task<IActionResult> Login(LoginDTO loginDTO, string? ReturnUrl)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Errors = ModelState.Values.SelectMany(x => x.Errors).Select(x => x.ErrorMessage);
+
+                return View(loginDTO);
+            }
+
+             var result = await _signInManager.PasswordSignInAsync(loginDTO.Email, loginDTO.Password, isPersistent: false, lockoutOnFailure: false);
+
+            if (result.Succeeded)
+            {
+                // Admin
+                ApplicationUser user = await _userManager.FindByEmailAsync(loginDTO.Email);
+
+                if (user != null) 
+                {
+                    if (await _userManager.IsInRoleAsync(user, UserTypeOptions.Admin.ToString()))
+                    {
+                        return RedirectToAction("Index", "Home", new { area = "Admin" });
+                    }
+                }
+
+                if (!string.IsNullOrEmpty(ReturnUrl) && Url.IsLocalUrl(ReturnUrl))
+                {
+                    return LocalRedirect(ReturnUrl);
+                }
+
+                return RedirectToAction(nameof(PersonsController.Index), "Persons");
+            }
+
+            ModelState.AddModelError("Login", "Invalid email or password");
+
+            return View(loginDTO);
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+
+            return RedirectToAction(nameof(PersonsController.Index), "Persons");
+        }
+
+        [AllowAnonymous]
+        public async Task<IActionResult> IsEmailAlreadyRegistered(string email)
+        {
+            ApplicationUser user = await _userManager.FindByEmailAsync(email);
+
+            if (user == null)
+            {
+                return Json(true); // valid
+            }
+            else
+            {
+                return Json(false); // invalid
+            }
+        }
+
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+    }
+}
